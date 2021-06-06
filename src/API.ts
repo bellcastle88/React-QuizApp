4ce753daf67d99cfa01a407d15c1af019c2f9fb8
Jace <bellcastle88@gmail.com>
2021-06-06 12:49:19 +0900

    f

--- src/API.ts
+++ src/API.ts
@@ -1,10 +1,28 @@
-export enum Difficulty {
-    EASY = "easy",
-    MEDIUM = "medium",
-    HARD = "hard",
-} 
+import { shuffleArray } from './utils';
 
-export const fetchQuizQestions = async (amount: number, difficulty: Difficulty) => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amout}$difficulty=${difficulty}$type=multiple`;
-    const data = await (await fetch(endpoint)).json();
-} 
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export type QuestionsState = Question & { answers: string[] };
+
+// Api 받아오기
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
+};
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,43 +1,45 @@
-import React from 'react'
+import React from 'react';
+// Types
+import { AnswerObject } from '../App';
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
-    question: string;
-    
-    // array타입 
-    answers: string[];
-    callback: any;
-    userAnswer: any;
-    questionNr: number;
-    totalQuestions: number;
-}
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
 
-// QuestionCard에 대한 타입작성 React.FC<Props>
-const QuestionCard: React.FC<Props> =({
-    question,
-    answers,
-    callback,
-    userAnswer,
-    questionNr,
-    totalQuestions,
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
 }) => (
+  <Wrapper>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-        <p className="number">
-            Question: {questionNr} / {totalQuestions}
-        </p>
-        <p dangerouslySetInnerHTML={{__html: question}}/>
-        <div>
-
-            {/* map = 반복문을 돌며 배열 안의 요소들을 1대1로 짝지어 주는 것 */}
-            {answers.map((answer) => (
-                <div>
-                    <button disabled={userAnswer} onClick={callback}>
-                        <span dangerouslySetInnerHTML={{__html: answer}}></span>
-                    </button>
-                </div>
-            ))}
-        </div>
-    
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
     </div>
+  </Wrapper>
 );
 
-export default QuestionCard
+export default QuestionCard;
